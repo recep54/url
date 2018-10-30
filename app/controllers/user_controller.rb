61a4bc5cb45befda4c385f1404c936cb8f149cf9
recep54 <recep54@outlook.fr>
2018-10-30 23:03:44 +0100

    first commit and pushing to heroku

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,24 +2,23 @@ class UserController < ApplicationController
   
 
   def new
-    @user = User.new
   end
 
   def create_post
-    @user = User.new
-    puts "hello"
-    puts params[:username]
-    puts params[:email]
-    puts params[:bio]
-    #User.create!(username: params[:username], email: params[:email], bio: params[:bio])
-    puts "tout est OK"
-    puts params
-    puts " ----------------"
-    puts params["person"]["username"]
-    puts params["person"]["email"]
-    puts params["person"]["bio"]
-    
-
+   
+  if params[:person]
+        # puts "god bye"
+        # puts params["person"]["username"]
+        # puts params["person"]["email"]
+        # puts params["person"]["bio"]
+        user = User.create!(username: params["person"]["username"], email:params["person"]["email"], bio: params["person"]["bio"])
+    else
+        # puts params[:username]
+        # puts params[:email]
+        # puts params[:bio]        
+        # puts "sa passe"
+        user = User.create!(username: params[:username], email: params[:email], bio: params[:bio])
+    end
  end
 
 
